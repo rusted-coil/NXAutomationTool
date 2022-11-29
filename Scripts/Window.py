6f5b4a62da336e840e381d4ab9ed2f49b4cf5381
rusted-coil <rusted.coil@gmail.com>
2022-11-29 18:14:38 +0900

    Qtでウィンドウを作成

--- Scripts/Window.py
+++ Scripts/Window.py
@@ -1,17 +1,21 @@
-class Window:
-    def __init__(self, master=None):
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+ 
+class Window(QWidget):
+    def __init__(self, parent = None):
+        super(Window, self).__init__(parent)
+
         # Window設定
-        master.title('NX Automation Tool')
-        master.geometry('1280x720')
+        self.resize(1280, 720)
+        self.setWindowTitle('NX Automation Tool')
 
-        # PainterViewを作成
-#        view = PainterView(master)
-
-        # PainterControllerを作成
-#        controller = PainterController(view)
-        
+        # サンプルレイアウト
+        btn = QPushButton('Hello World PyQt5', self) # ボタンウィジェット作成
+        btn.resize(btn.sizeHint()) # ボタンのサイズの自動設定
+        btn.move(100, 50) # ボタンの位置設定(ボタンの左上の座標)
+ 
 if __name__ == '__main__':
-    import tkinter as tk
-    root = tk.Tk()
-    app = Window(root)
-    root.mainloop()
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
