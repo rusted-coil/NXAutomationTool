9c59b488ba143a8bea2ad87777911679ec641484
rusted-coil <rusted.coil@gmail.com>
2022-11-29 19:11:27 +0900

    Qtのuiファイル読み込みテスト

--- Scripts/Window.py
+++ Scripts/Window.py
@@ -1,18 +1,21 @@
 import sys
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
  
 class Window(QWidget):
     def __init__(self, parent = None):
         super(Window, self).__init__(parent)
 
+        uic.loadUi('form.ui', self)
+
         # Window設定
-        self.resize(1280, 720)
-        self.setWindowTitle('NX Automation Tool')
+#        self.resize(1280, 720)
+#        self.setWindowTitle('NX Automation Tool')
 
         # サンプルレイアウト
-        btn = QPushButton('Hello World PyQt5', self) # ボタンウィジェット作成
-        btn.resize(btn.sizeHint()) # ボタンのサイズの自動設定
-        btn.move(100, 50) # ボタンの位置設定(ボタンの左上の座標)
+#        btn = QPushButton('Hello World PyQt5', self) # ボタンウィジェット作成
+#        btn.resize(btn.sizeHint()) # ボタンのサイズの自動設定
+#        btn.move(100, 50) # ボタンの位置設定(ボタンの左上の座標)
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
